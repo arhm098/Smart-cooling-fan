97bda2f7273460c4d06885cace1e884307fafe55
arhm098 <105104508+arhm098@users.noreply.github.com>
2023-01-16 00:17:28 +0500

    shit

---
+++ temptest/CPUtest.py
@@ -0,0 +1,16 @@
+import clr # the pythonnet module.
+clr.AddReference(r'OpenHardwareMonitorLib') 
+# e.g. clr.AddReference(r'OpenHardwareMonitor/OpenHardwareMonitorLib'), without .dll
+
+from OpenHardwareMonitor.Hardware import Computer
+
+c = Computer()
+c.CPUEnabled = True # get the Info about CPU
+c.GPUEnabled = True # get the Info about GPU
+c.Open()
+while True:
+    for a in range(0, len(c.Hardware[0].Sensors)):
+        # print(c.Hardware[0].Sensors[a].Identifier)
+        if "/temperature" in str(c.Hardware[0].Sensors[a].Identifier):
+            print(c.Hardware[0].Sensors[a].get_Value())
+            c.Hardware[0].Update()
