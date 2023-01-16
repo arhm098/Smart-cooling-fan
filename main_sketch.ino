ffb72f3b822f5809f06856301d6979845ff0b4f6
arhm098 <105104508+arhm098@users.noreply.github.com>
2023-01-16 18:28:55 +0500

    yelo zaka

---
+++ main_sketch.ino
@@ -0,0 +1,7 @@
+//display room temp in first half of display
+//display forcast in second half
+//attach the motor with two fans, which are connected through USB
+
+// 20C - 30C 0%-100% cooling pad speed
+//above 85% humidity turn on relay
+
